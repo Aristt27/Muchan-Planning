394a5a8b3ad99bc227c307257fe11f1799f98e84
Aristt27 <37422849+Aristt27@users.noreply.github.com>
2019-07-29 16:05:00 -0300

    Add files via upload

---
+++ source/script.js
@@ -0,0 +1,744 @@
+class Point {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+  }
+
+  move(v){
+    this.x += v.x;
+    this.y += v.y;
+  }
+
+  update(mX,mY){
+    return;
+  }
+
+  move_to(p){
+    this.x = p.x;
+    this.y = p.y;
+  }
+}
+
+class Robot {
+  constructor(robots, root_point, color=random_color()) {
+    this.color = color;
+    this.points = [root_point];
+    this.open = true;
+    this.grab_hook = null;
+    robots.push(this);
+  }
+
+  get_center(){
+    let s = new Point(0,0);
+    let i;
+    for (i=0; i<this.points.length; i++){
+      s.x += this.points[i].x;
+      s.y += this.points[i].y;
+    }
+    s.x /= this.points.length;
+    s.y /= this.points.length;
+    return s;
+  }
+
+  update(mX,mY){
+    return;
+  }
+
+  get_grabbed_at(p){
+    this.grab_hook = p;
+    grabbed = this;
+  }
+
+  move_to(p){
+    let v = createVector(p.x - this.grab_hook.x, p.y - this.grab_hook.y);
+    this.move_points(v);
+    this.grab_hook = p;
+  }
+
+  move(vec){
+    this.move_points(vec);
+  }
+
+  move_points(vec){
+    let dx = vec.x, dy = vec.y;
+    let i;
+    for (i=0; i<this.points.length; i++){
+      this.points[i].move(vec);
+    }
+  }
+
+  addPoint(_point_) {
+    this.points.push(_point_);
+  }
+
+  finish(){
+    this.open = false;
+  }
+
+  draw(){
+    if(this.open){
+      this.open_draw();
+    } else {
+      this.closed_draw();
+    }
+    let center = this.get_center();
+    debugable_point(center.x, center.y);
+  }
+
+  open_draw() {
+    let i;
+    let _point_;
+    let mX = mouseX, mY = mouseY;
+    push();
+    beginShape();
+    fill(this.color);
+    for (i = 0; i < this.points.length; i++) {
+      _point_ = this.points[i];
+      vertex(_point_.x, _point_.y);
+      debugable_point(_point_.x, _point_.y);
+    }
+    vertex(mX, mY);
+    debugable_point(mX, mY);
+    endShape();
+    pop();
+  }
+
+  closed_draw() {
+    let i;
+    let c = color(255,0,0)
+    let _point_;
+    beginShape();
+    fill(this.color);
+    for (i = 0; i < this.points.length; i++) {
+      _point_ = this.points[i];
+      vertex(_point_.x, _point_.y);
+      grabbable_point(_point_);
+      debugable_point(_point_.x, _point_.y);
+    }
+    debugable_point(_point_.x,_point_.y);
+    endShape(CLOSE);
+    let center = this.get_center();
+    _point_ = center;
+    fill(c)
+    ellipse(_point_.x,_point_.y,5,5);
+  }
+  is_inside(p) {
+    let c = this.get_center();
+    let v = createVector(c.x-p.x, c.y-p.y);
+    v.normalize();
+    // count how many times v intersects this shape.
+    let pseudo_ray = {x:p.x, y:p.y, vec:v};
+    let intersections = get_intersections(pseudo_ray, this);
+    let n = intersections.length;
+    return !!((n>0) && (n%2));
+  }
+}
+
+class Obstacle {
+  constructor(obstacles, root_point, color=random_color()) {
+    this.color = color;
+    this.points = [root_point];
+    this.open = true;
+    this.grab_hook = null;
+    obstacles.push(this);
+  }
+
+  get_center(){
+    let s = new Point(0,0);
+    let i;
+    for (i=0; i<this.points.length; i++){
+      s.x += this.points[i].x;
+      s.y += this.points[i].y;
+    }
+    s.x /= this.points.length;
+    s.y /= this.points.length;
+    return s;
+  }
+
+  update(mX,mY){
+    return;
+  }
+
+  get_grabbed_at(p){
+    this.grab_hook = p;
+    grabbed = this;
+  }
+
+  move_to(p){
+    let v = createVector(p.x - this.grab_hook.x, p.y - this.grab_hook.y);
+    this.move_points(v);
+    this.grab_hook = p;
+  }
+
+  move(vec){
+    this.move_points(vec);
+  }
+
+  move_points(vec){
+    let dx = vec.x, dy = vec.y;
+    let i;
+    for (i=0; i<this.points.length; i++){
+      this.points[i].move(vec);
+    }
+  }
+
+  addPoint(_point_) {
+    this.points.push(_point_);
+  }
+
+  finish(){
+    this.open = false;
+  }
+
+  draw(){
+    if(this.open){
+      this.open_draw();
+    } else {
+      this.closed_draw();
+    }
+    let center = this.get_center();
+    debugable_point(center.x, center.y);
+  }
+
+  open_draw() {
+    let i;
+    let _point_;
+    let mX = mouseX, mY = mouseY;
+    push();
+    beginShape();
+    fill(this.color);
+    for (i = 0; i < this.points.length; i++) {
+      _point_ = this.points[i];
+      vertex(_point_.x, _point_.y);
+      debugable_point(_point_.x, _point_.y);
+    }
+    vertex(mX, mY);
+    debugable_point(mX, mY);
+    endShape();
+    pop();
+  }
+
+  closed_draw() {
+    let i;
+    let _point_;
+    beginShape();
+    fill(this.color);
+    for (i = 0; i < this.points.length; i++) {
+      _point_ = this.points[i];
+      vertex(_point_.x, _point_.y);
+      grabbable_point(_point_);
+      debugable_point(_point_.x, _point_.y);
+    }
+    endShape(CLOSE);
+  }
+  is_inside(p) {
+    let c = this.get_center();
+    let v = createVector(c.x-p.x, c.y-p.y);
+    v.normalize();
+    // count how many times v intersects this shape.
+    let pseudo_ray = {x:p.x, y:p.y, vec:v};
+    let intersections = get_intersections(pseudo_ray, this);
+    let n = intersections.length;
+    return !!((n>0) && (n%2));
+  }
+}
+
+// Modes
+let debug_mode = false;
+
+let robot_mode = 1;
+let obstacle_mode = 2;
+let edit_mode = 3;
+let finish_line_mode = 12;
+let finishline = null
+
+let draw_mode = obstacle_mode;
+
+let drawing_obstacle = false;
+let drawing_robot = false;
+
+let obstacles = [];
+let robots = [];
+
+// Editing
+let grabbable_points = [];
+let grabbing_radius = 5;
+let grabbed = null;
+
+// Window
+let h = 720, w = 1080;
+let canvas_diagonal = Math.sqrt(h**2 + w**2);;
+
+// Color Management
+
+
+
+function randint(a, b) {
+  // random integer in [a, b]
+  if (typeof b == 'undefined'){
+    b = a;
+    a = 0;
+  }
+  return a + Math.floor((b-a)*Math.random());
+}
+
+function random_color(_alpha_=255) {
+  let r = randint(0,255);
+  let g = randint(0,255);
+  let b = randint(0,255);
+  return color(r, g, b, _alpha_);
+}
+
+function intense_color(_alpha_=255) {
+  let r = randint(0,255);
+  let g = randint(0,255);
+  let b = 255 - ((r+g)/2);
+  return color(r, g, b, _alpha_);
+}
+
+function last(x){
+  return x[x.length-1];
+}
+
+function last_obstacle(obstacles){
+  return last(obstacles);
+}
+function last_robot(robots){
+  return last(robots)
+}
+
+function draw_obstacles(obstacles){
+  let i;
+  for (i=0; i < obstacles.length; i++){
+    obstacles[i].draw();
+  }
+}
+
+function set_mode(_mode_) {
+  if (_mode_ != draw_mode) {
+    cancel_draw(draw_mode);
+    draw_mode = _mode_;
+  }
+}
+
+function draw_robots(robots){
+  if (robots[0] != null){
+    robots[0].draw();
+  }
+}
+
+function hypot(a, b){
+  // Hypotenuse
+  return Math.sqrt(a**2 + b**2);
+}
+
+function mouse_inside(x1=0,y1=0,x2=width,y2=height){
+  let mX = mouseX, mY = mouseY;
+  return ((x1 <= mX) && (mX <= x2) && (y1 <= mY) && (mY <= y2));
+}
+
+function draw_cursor(draw_mode){
+  switch(draw_mode){
+    case obstacle_mode:
+      cursor(CROSS);
+      break;
+    case finish_line_mode:
+      cursor(TEXT);
+      break;
+    case robot_mode:
+      cursor(WAIT);
+      break;
+    case edit_mode:
+      if (mouseIsPressed){
+        cursor('grabbing');
+      } else {
+      cursor('grab');
+      }
+      break;
+  }
+}
+
+function debugable_point(x,y) {
+  if(debug_mode){
+    push();
+    fill(255,100,100);
+    show_coords(x, y);
+    pop();
+  }
+}
+
+// p5.js loops
+function setup(){
+    // setup window
+    createCanvas(w,h);
+}
+
+function draw() {
+  // Clear screen
+  background(255);
+
+  // Cursor
+  draw_cursor(draw_mode);
+
+  // Grabbable Points
+  grabbable_points = [];
+
+  // Draw objects
+  draw_obstacles(obstacles);
+  draw_robots(robots);
+  if (finishline != null){
+    let d = color(0,255,0);
+    fill(d);
+    ellipse(finishline[0],finishline[1],5,5);
+
+  }
+  // move grabbed objects
+  move_grab();
+}
+
+// button actions
+
+function _save_(_path_, _mode_='png') {
+  // Future: save only the smallest rectangle around the shapes.
+  saveCanvas(_path_, _mode_);
+}
+
+function toggle_debug_mode(img){
+  if(debug_mode){
+    debug_mode = false;
+    document.getElementById("debug_img").src = "../Trabalho 1/images/debug2.png";
+  } else{
+    debug_mode = true;
+    document.getElementById("debug_img").src = "../Trabalho 1/images/debug.png";
+  }
+}
+
+// coordinate_strings
+function coords(x,y,prec=1){
+  return "["+x.toFixed(prec)+ ","+y.toFixed(prec)+"]"
+}
+
+function show_coords(x,y,dx=0,dy=0,prec=1){
+  text(coords(x,y,prec),x+dx,y+dy);
+}
+
+
+// Cancel drawing
+
+function drawing(){
+  return (drawing_robot || drawing_obstacle);
+}
+
+function cancel_draw(target){
+  // Cancel the shape drawing
+  if (drawing()){
+    let figures;
+    switch(target){
+      case robot_mode:
+        drawing_robot = false;
+        figures = robots;
+        break;
+      case obstacle_mode:
+        drawing_obstacle = false;
+        figures = obstacles;
+        break;
+      }
+    let _figure_ = figures.pop();
+    _figure_ = undefined;
+    delete _figure_;
+  }
+}
+
+function force_finish(target){
+  if (drawing()){
+    switch(target){
+
+      case robot_mode:
+        _robot_ = last_robot(robots);
+        _robot_.finish();
+        drawing_robot = false;
+        break;
+      case obstacle_mode:
+        _obstacle_ = last_obstacle(obstacles);
+        _obstacle_.finish();
+        drawing_obstacle = false;
+        break;
+
+    }
+  }
+}
+
+// events
+
+function mouseClicked() {
+  if (!mouse_inside()){
+    return;
+  }
+  let _point_;
+  switch(draw_mode){
+    case finish_line_mode:
+      finishline = [mouseX,mouseY];
+      break;
+    case obstacle_mode:
+      let _obstacle_;
+      if (drawing_obstacle){
+        // Drawing new obstacle
+        _point_ = new Point(mouseX, mouseY);
+        _obstacle_ = last_obstacle(obstacles);
+        _obstacle_.addPoint(_point_);
+      } else {
+        // Not Drawing
+        _point_ = new Point(mouseX, mouseY);
+        _obstacle_ = new Obstacle(obstacles, _point_, random_color(128));
+        drawing_obstacle = true;
+      }
+      break;
+      case robot_mode:
+        let _robot_;
+        if (drawing_robot){
+          // Drawing new robot
+          _point_ = new Point(mouseX, mouseY);
+          _robot_ = last_robot(robots);
+          _robot_.addPoint(_point_);
+        } else {
+          // Not Drawing
+          _point_ = new Point(mouseX, mouseY);
+          _robot_ = new Robot(robots, _point_, random_color(128));
+          drawing_robot = true;
+        }
+    break;
+    case edit_mode:
+      break;
+    case finish_line_mode:
+      break;
+  }
+}
+
+function doubleClicked() {
+  if (!mouse_inside()){
+    return;
+  }
+  switch(draw_mode){
+    case obstacle_mode:
+      if (drawing_obstacle){
+        // end shape
+        let _obstacle_ = last_obstacle(obstacles);
+        _obstacle_.finish();
+        _obstacle_.points.pop();
+        drawing_obstacle = false;
+      } else {
+        // do nothing
+        return;
+      }
+      break;
+    case robot_mode:
+      if (drawing_robot){
+        // end shape
+        let _robot_ = last_robot(robots);
+        _robot_.finish();
+        _robot_.points.pop();
+        drawing_robot = false;
+      } else {
+        // do nothing
+        return;
+      }
+      break;
+    case edit_mode:
+      break;
+    case finish_line_mode:
+      break;
+    default:
+      return;
+  }
+}
+
+function keyPressed() {
+  switch(keyCode){
+    case ESCAPE:
+      // Cancel drawing
+      cancel_draw(draw_mode);
+      break;
+
+    case ENTER:
+      // force finishing the shape drawing
+      force_finish(draw_mode);
+      break;
+  }
+}   //TODO
+
+function mousePressed() {
+  grab();
+}
+
+function mouseReleased(){
+  release_grab();
+}
+
+// Grabbing Folks
+
+function grabbable_point(p){
+  grabbable_points.push(p);
+  if (draw_mode == edit_mode){
+    push();
+    fill(0);
+    ellipse(p.x, p.y, grabbing_radius);
+    pop();
+  }
+}
+
+function grab(){
+  if(draw_mode == edit_mode){
+    let mX = mouseX, mY = mouseY;
+    let i;
+    let _point_;
+    let s = grabbing_radius, p = null, d;
+    for(i=0; i<grabbable_points.length; i++){
+      _point_ = grabbable_points[i];
+      d = hypot(_point_.x-mX, _point_.y-mY);
+      if (d <= s){
+        s = d;
+        p = _point_;
+      }
+    }
+    // now `p` is the nearest point to the click!
+    grabbed = p;
+
+    // or not...
+    if (grabbed === null){
+      // everbody needs a second chance. (confucius 551-479 b.c.)
+      let mp = new Point(mX, mY);
+      for (i=0; i<obstacles.length; i++){
+        if (obstacles[i].is_inside(mp)){
+          obstacles[i].get_grabbed_at(mp);
+        }
+      }
+      for (i=0; i<robots.length; i++){
+        if (robots[i].is_inside(mp)){
+          robots[i].get_grabbed_at(mp);
+        }
+      }
+    }
+  }
+}
+
+function release_grab(){
+  grabbed = null;
+}
+
+function move_grab(){
+  if (!(grabbed === null)){
+    p = new Point(mouseX, mouseY);
+    grabbed.move_to(p);
+  }
+}
+// intersections
+
+let symbols_outer = 8;
+let symbols_inner = 3;
+
+function draw_entering(p){
+  push();
+  fill(255);
+  ellipse(p.x, p.y, symbols_outer);
+  strokeWeight(1.5)
+  line(p.x-symbols_inner,p.y-symbols_inner,p.x+symbols_inner,p.y+symbols_inner);
+  line(p.x-symbols_inner,p.y+symbols_inner,p.x+symbols_inner,p.y-symbols_inner);
+  pop();
+}
+
+function draw_exiting(p){
+  push();
+  fill(255);
+  ellipse(p.x, p.y, symbols_outer);
+  fill(0);
+  ellipse(p.x, p.y, symbols_inner);
+  pop();
+}
+
+function sort_intersections(its){
+  // Bubble Sorting Intersections by distance from source ray.
+  let intersections = [];
+  let i,j;
+  let n = its.length-1;
+  let flag = true;
+  while ((n > 0)  && flag){
+    flag = false;
+    for (j=0; j<n; j++){
+      if(its[j].d > its[j+1].d){
+        let temp = its[j];
+        its[j] = its[j+1];
+        its[j+1] = temp;
+        flag = true;
+      }
+    }
+    n--;
+  }
+  for(i=0; i<its.length; i++){
+    intersections.push(its[i].p);
+  }
+  return intersections;
+}
+
+function get_intersections(ray, shape){
+  let intersections = [];
+  let s = ray;
+  let v = ray.vec;
+  let av = v.y/v.x;
+
+  let points = shape.points;
+  let p1 = points[0];
+
+  for(k=1; k<=points.length; k++){
+    let p2 = points[k%points.length];
+    let px = p2.x - p1.x, py = p2.y - p1.y;
+    let p = createVector(px, py);
+
+    let ap = py/px;
+    let x = (av*s.x - s.y - ap*p1.x + p1.y)/(av - ap);
+    let y = av*(x - s.x) + s.y;
+
+    let u = createVector(x-s.x, y-s.y);
+    let w = createVector(x-p1.x, y-p1.y);
+
+    let m = p5.Vector.dot(p, w);
+    let c = p5.Vector.dot(p, p);
+
+    if(p5.Vector.dot(u, v) > 0){
+      if(0<=m && m<=c){
+        // intersection found
+        intersections.push({p: new Point(x,y), d:u.mag()});
+      }
+    }
+    p1 = p2;
+  }
+  return sort_intersections(intersections);
+}
+
+function all_intersections(rays, shapes){
+  // rays, shapes -> object list : [{x: (Int), y: (Int), ray: (Ray), shape: (Shape)}, ... ]
+  let intersections = [];
+  let i,j,k;
+  for(i=0; i<rays.length; i++){
+    for(j=0; j<shapes.length; j++){
+      intersections.push(get_intersections(rays[i],shapes[j]));
+    }
+  }
+  return intersections;
+}
+
+function draw_intersections(rays, shapes){
+  let intersections = all_intersections(rays, shapes);
+  let i,j;
+  for (i=0; i<intersections.length; i++){
+    let n = intersections[i].length;
+    for (j=0; j<n; j++){
+      if(!((n%2) ^ (j%2))){
+        // Entering shape
+        draw_entering(intersections[i][j]);
+      } else{
+        // Exiting shape
+        draw_exiting(intersections[i][j]);
+      }
+    }
+  }
+}
